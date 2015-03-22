585570ddf17af1def7142922e8f65a31c3f8ebdd
Martin Vieth <martin_123_vieth@hotmail.com>
2015-03-22 14:27:53 +0100

    Client virker/giver ikke fejl yay

--- src/UDP.c
+++ src/UDP.c
@@ -10,21 +10,24 @@ int main(void){
 	struct sockaddr_in servAddr;
 	struct sockaddr_in clntAddr;
 	int clntAddrLen;
-	int server_port = 8001;
+	int server_port = 8000;
 	//Build local(server) socket address
 	memset(&servAddr,0,sizeof(servAddr));
 	servAddr.sin_family = AF_INET;
 	servAddr.sin_port = htonl(server_port);
 	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
 	//create socket
-	if((PF_INET,SOCK_DGRAM,0)< 0)
+	if((s = socket(PF_INET,SOCK_DGRAM,0))< 0)
 	{
 		perror("ERROR:socket failed!");
 		exit(1);
 	}
 	//Bind socket to local address and port
-	if ((bind(s,(struct sockaddr*)&servAddr,sizeof(servAddr))<0)){
+	if(bind(s,(struct sockaddr*)&servAddr, sizeof(servAddr)) < 0){
+
 		perror("ERROR: bind failed!");
+		//close(-1);
 		exit(1);
 	}
 	for(;;)  // Run forever
